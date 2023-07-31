dbc794a109a2b5eef40f8075b3ea3fb6cf402e29
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-07-31 19:59:04 +0700

    upbai

--- angular-calculator-app/src/app/app.module.ts
+++ angular-calculator-app/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import {FormsModule} from '@angular/forms';
 import { ColorComponent } from './color/color.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    ColorComponent
+    ColorComponent,
+    ArticleComponent,
+    LikeComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
