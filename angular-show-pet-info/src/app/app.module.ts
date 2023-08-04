56811c67aa69c4969adc870a4ab477a5d277db6c
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-08-04 14:59:19 +0700

    up

--- angular-show-pet-info/src/app/app.module.ts
+++ angular-show-pet-info/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetComponent } from './pet/pet.component';
 import {FormsModule} from '@angular/forms';
+import { NameCardComponent } from './name-card/name-card.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PetComponent,
+    NameCardComponent,
+    ProgressBarComponent,
+    CountdownTimerComponent,
   ],
     imports: [
         BrowserModule,
