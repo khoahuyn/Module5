0485aa5d39b234ea921fc6774f72d468de94ab79
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-08-06 23:14:59 +0700

    upbai

--- Furama-app/src/app/addcontract/addcontract.component.ts
+++ Furama-app/src/app/addcontract/addcontract.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {datePatternValidator} from '../addcustomer/date-pattern.validator';
 
 @Component({
   selector: 'app-addcontract',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addcontract.component.css']
 })
 export class AddcontractComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(6)]],
+      serviceName: ['', [Validators.required, Validators.minLength(6)]],
+      dayCreate: ['', [Validators.required, datePatternValidator]],
+      endDay: ['', [Validators.required, datePatternValidator]],
+      deposit: ['', [Validators.required, Validators.min(1000)]],
+      total: ['', [Validators.required, Validators.min(2000)]],
+    });
+  }
+
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
+  }
 }
