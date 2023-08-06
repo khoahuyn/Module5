98ad99dc10537c5d14159701346fa1ef54e1870f
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-08-06 15:21:04 +0700

    upbai

--- angular-show-pet-info/src/app/register-form/register-form.component.ts
+++ angular-show-pet-info/src/app/register-form/register-form.component.ts
@@ -8,21 +8,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterFormComponent implements OnInit {
 
+  submitted = false;
+
   registerForm: FormGroup;
+
+
+
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-      country: [ '', Validators.required],
-      age: [ '', Validators.required],
-      gender: [ '', Validators.required],
-      phone: [ '', Validators.required]
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      country: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')]]
     });
   }
+
 
   onSubmit(): void {
     if (this.registerForm.valid) {
